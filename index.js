a8d5cdd3defcd61aeb30e3bb22d45becf834fd9f
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-09-15 11:44:45 -0300

    [add] env.PORT

--- index.js
+++ index.js
@@ -13,18 +13,17 @@ const authMiddleware = require("./middleware/authMiddleware");
 const app = express();
 app.use(bodyParser.json());
 
-app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if(req.method ==='OPTIONS'){
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
-})
+});
 
 app.use(authMiddleware);
-
 
 app.use(
   "/graphql",
@@ -39,6 +38,6 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-lotux.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(8000);
+    app.listen(process.env.PORT || 8000);
   })
   .catch((err) => console.log(err));
